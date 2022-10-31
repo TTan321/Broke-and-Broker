f38c6e049c5d6905a03ac94d2a57c45ee1046b6e
TonyTan <tonytan2193@gmail.com>
2022-10-30 22:08:59 -0700

    Styled login and sing up pages

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
 import './LoginForm.css'
 
 const LoginForm = () => {
+  const dispatch = useDispatch();
+  const history = useHistory()
+  const user = useSelector(state => state.session.user);
+
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -33,36 +35,41 @@ const LoginForm = () => {
 
   return (
     <div id='loginFormPage'>
-      <div id='loginFormImage'>IMG</div>
-      <form id='loginForm' onSubmit={onLogin} >
-        Login Form
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-          />
-          <button type='submit'>Login</button>
-        </div>
-      </form>
+      <div id='loginFormImage'>
+        <img id='moonImage' src='https://images.unsplash.com/photo-1446941611757-91d2c3bd3d45?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=802&q=80' />
+      </div>
+      <div className='formContainer'>
+        <form id='loginForm' onSubmit={onLogin} >
+          <p id='loginHeader'>Log In Here</p>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            <label className='loginLabels' htmlFor='email'><p className='labelText'>Email</p></label>
+            <input
+              className='loginInputs'
+              name='email'
+              type='text'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <label className='loginLabels' htmlFor='password'><p className='labelText'>Password</p></label>
+            <input
+              className='loginInputs'
+              name='password'
+              type='password'
+              value={password}
+              onChange={updatePassword}
+            />
+            <button id='loginButton' type='submit'>Log In</button>
+            <p id='signUpText'>Not on Moon Rocket? <span id='signUpRedirect' onClick={() => history.push('/sign-up')}>Create an account</span></p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import './SignUpForm.css'
 
 const SignUpForm = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector(state => state.session.user);
+
   const [errors, setErrors] = useState([]);
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
@@ -21,6 +25,14 @@ const SignUpForm = () => {
         setErrors(data)
       }
     }
+  };
+
+  const updateFirstname = (e) => {
+    setFirstname(e.target.value);
+  };
+
+  const updateLastname = (e) => {
+    setLastname(e.target.value);
   };
 
   const updateUsername = (e) => {
@@ -45,53 +57,84 @@ const SignUpForm = () => {
 
   return (
     <div id='SignUpFormPage'>
-      <div id='SignUpFormImg'>IMG</div>
-      <form id='SignUpForm' onSubmit={onSignUp}>
-        Sign Up Form
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
-        </div>
-        <div>
-          <label>User Name</label>
-          <input
-            type='text'
-            name='username'
-            onChange={updateUsername}
-            value={username}
-          ></input>
-        </div>
-        <div>
-          <label>Email</label>
-          <input
-            type='text'
-            name='email'
-            onChange={updateEmail}
-            value={email}
-          ></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type='password'
-            name='password'
-            onChange={updatePassword}
-            value={password}
-          ></input>
-        </div>
-        <div>
-          <label>Repeat Password</label>
-          <input
-            type='password'
-            name='repeat_password'
-            onChange={updateRepeatPassword}
-            value={repeatPassword}
-            required={true}
-          ></input>
-        </div>
-        <button type='submit'>Sign Up</button>
-      </form>
+      <div id='SignUpFormImg'>
+        <img id='rocketImage' src='https://images.unsplash.com/photo-1636828982375-a4ec8b809e5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHJvY2tldCUyMGxhdW5jaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60' />
+      </div>
+      <div className='formContainer'>
+        <form id='SignUpForm' onSubmit={onSignUp}>
+          <p id='signUpHeader'>Sign Up Here</p>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div id='namesContainer'>
+            <div>
+              <input
+                className='signUpInputNames'
+                type='text'
+                name='firstname'
+                onChange={updateFirstname}
+                value={firstname}
+                placeholder='First name'
+              ></input>
+            </div>
+            <div>
+              <input
+                className='signUpInputNames'
+                type='text'
+                name='lastname'
+                onChange={updateLastname}
+                value={lastname}
+                placeholder='Last name'
+              ></input>
+            </div>
+          </div>
+          <div>
+            <input
+              className='signUpFormInputs'
+              type='text'
+              name='username'
+              onChange={updateUsername}
+              value={username}
+              placeholder='Username'
+            ></input>
+          </div>
+          <div>
+            <input
+              className='signUpFormInputs'
+              type='text'
+              name='email'
+              onChange={updateEmail}
+              value={email}
+              placeholder='Email address'
+            ></input>
+          </div>
+          <div>
+            <input
+              className='signUpFormInputs'
+              type='password'
+              name='password'
+              onChange={updatePassword}
+              value={password}
+              placeholder='Password'
+            ></input>
+          </div>
+          <div>
+            <input
+              className='signUpFormInputs'
+              type='password'
+              name='repeat_password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+              placeholder='Confirm password'
+            ></input>
+          </div>
+          <button id='signUpButton' type='submit'>Sign Up</button>
+          <p id='loginText'>Already have an account? <span id='loginRedirect' onClick={() => history.push('/login')}>Log in</span></p>
+        </form>
+      </div>
     </div>
   );
 };
